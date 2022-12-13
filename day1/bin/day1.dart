07d6e9953be9fb8bf32216ff3352effaefcbef4c
SAIFUL <si87878@gmail.com>
2022-12-13 07:18:11 +0600

    int manipulation

--- day1/bin/day1.dart
+++ day1/bin/day1.dart
@@ -2,9 +2,13 @@ import 'package:day1/day1.dart' as day1;
 
 void main() {
   int a = 10;
-  int b = 5;
+  int b = 4;
+  int c;
+  c = a % b;
   print(a + b + day1.calculate());
   print(day1.calculate() - a);
   print(a * b * day1.calculate());
   print((a * day1.calculate()) ~/ b);
+  print(b / a);
+  print(c);
 }
