affd239e957a6ef75bee2c489c052a7ab91921a0
SAIFUL <si87878@gmail.com>
2022-12-13 22:52:40 +0600

    string qutoation

--- day1/bin/day1.dart
+++ day1/bin/day1.dart
@@ -11,17 +11,11 @@ void main() {
   print((a * day1.calculate()) / b);
   print(b / a);
   print(c);
-  
-  print(a*a+2*a*b+b*b);
 
-<<<<<<< HEAD
-  double pie = 3.1416;
-  print(4 / 2 + 3);
+  print(a * a + 2 * a * b + b * b);
 
   //string Manipulation
   String str1 = ' he says "i am learning dart."';
   String str2 = " he says 'i am learning dart.'";
   print(str1 + ' ' + str2);
-=======
->>>>>>> 413ce8c3f1b147e8c77ac50b78207f5184db28ed
 }
